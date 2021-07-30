9ad80daa8278d1e87994939be307f7ac72aa0c16
howard chang <idhowardgj94@gmail.com>
2021-07-30 17:41:26 +0800

    Done. Mind blown...

--- lens-prove/app/Main.hs
+++ lens-prove/app/Main.hs
@@ -9,11 +9,12 @@ import Lib.OldFashionLens
 import Data.Text
 import Model
 import Lib.Lens
-
+import Lib.LensProgress
 main :: IO ()
 main = do
     putStrLn "basic use: "
     basic
     useModifyFunc
     oldStyleLenses
-    useLaarhovenLenses
+    useLensPregress
+    testLens 
--- lens-prove/src/Lib/Lens.hs
+++ lens-prove/src/Lib/Lens.hs
@@ -1,80 +1,43 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveFunctor #-}
-{-# OPTIONS_GHC -Wno-deferred-type-errors #-}
 
--- Van Laarhoven lense
--- :)
-
+-- let put it all together
 module Lib.Lens where
 
 import Model
-import Lib.Basic
-import Lib
 
--- goal
--- 1. combine getter and modifier into a single value, without using product type.
+testLens = putStrLn "testLens"
 
--- ==== type signature ===
--- type Lens s a = <......>
--- view :: Lens s a -> s -> a
--- over :: Lens s a -> (a -> a) -> s -> s 
--- =======================
-
--- wrap type is a common skill in fp 
+type Lens s a = forall f. Functor f => (a -> f a) -> (s -> f s)
+-- wrapper type
 newtype Identity a = Identity { runIdentity :: a } deriving (Functor, Show)
+newtype Const a b = Const { getConst :: a } deriving (Functor, Show)
 
-type LensModify s a = (a -> Identity a) -> (s -> Identity s)
-
-over :: LensModify s a -> (a -> a) -> s -> s
+-- getter and setter
+over :: Lens s a -> (a -> a) -> s -> s
 over lens f s = runIdentity (lens (Identity . f) s)
---  Identity . f :: Identity $ (a -> a) => (a -> Identity a) 
---  lens (Identity . f) :: s -> Identity s
---  lens (Identity . f) s :: Identity s #
---  SL means setter lens.
---  GL means getter lens.
-personAddressSL :: LensModify Person Address
-personAddressSL f person = Identity $ person
-    { personAddress = runIdentity $ f $ personAddress person
-    }
--- or use Functor:
--- fmap :: (a -> b) -> f a -> f b
-personAddressSL' :: LensModify Person Address
--- f :: a -> Identity a 
-personAddressSL' f person =
+
+view :: Lens s a -> s -> a
+view lens s = getConst (lens Const s)
+
+personAddressL :: Lens Person Address
+personAddressL f person =
     (\address -> person { personAddress = address })
     <$> f (personAddress person)
 
-modifyPersonAddress' :: (Address -> Address) -> Person -> Person
-modifyPersonAddress' = over personAddressSL'
+getPersonAddress :: Person -> Address
+getPersonAddress = view personAddressL
 
--- view :: Lens s a -> s -> a
-newtype Const a b = Const { getConst :: a }
-type LensGetter s a = s -> Const a s
+modifyPersonAddress :: (Address -> Address) -> Person -> Person
+modifyPersonAddress = over personAddressL
 
-view :: LensGetter s a -> s -> a
-view lens s = getConst (lens s)
+setPersonAddress :: Address -> Person -> Person
+setPersonAddress address = modifyPersonAddress (const address)
 
-personAddressGL :: LensGetter Person Address
-personAddressGL person = Const (personAddress person )
+-- compose lens
+lens :: (s -> a) -> (s -> a -> s) -> Lens s a
+lens getter setter f s = setter s <$> f (getter s)
 
--- type LensModify s a = (a -> Identity a) -> (s -> Identity s)
--- type LensGetter s a = s -> Const a s
--- we need to align the interface, so we redefine a new LensGetter
-type LensGetter' s a = (a -> Const a a) -> (s -> Const a s)
-view' :: LensGetter' s a -> s -> a 
-view' lens s = getConst (lens Const s)
 
-personAddressGL' f person = Const $ getConst $ f (personAddress person)
 
--- TODO: practice -> personAddressGL to functor implementation
-useLaarhovenLenses :: IO ()
-useLaarhovenLenses = do
-    putStrLn "===== use Laarhoven Lenses ====="
-    let address = Address { addressCity = "Taipei", addressStreet = "Luosifu Road" }
-    let person = Person { personAddress = address, personName = "Dager" }
-    let correctp = modifyPersonAddress' (\address -> address { addressCity = "Xinbei" } ) person
-    putStrLn ""
-    putStrLn "here is ModifyLens:"
-    putStrLn "person: " <> print person
-    putStrLn "modify city to Xinbei use Functor version of personAddressL'" <> print correctp
